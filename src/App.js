cf0a0c6070f92dc4fa1a7a2e16e3b95508fb634d
Jorge <floreshfj@gmail.com>
2023-05-10 18:31:49 -0400

    rating de estrellas

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import StarRating from './starRating';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+        
+        <div><StarRating/></div>
+    </div>
+  );
+}
+
+export default App;
